bdafec9591b28e143de3ecc3f2475efa90635fb4
Tunji Ruwase <olruwase@microsoft.com>
2021-08-19 19:03:42 +0000

    Use manual mocks

--- docs/code-docs/source/conf.py
+++ docs/code-docs/source/conf.py
@@ -80,3 +80,7 @@ add_module_names = True
 autoclass_content = 'both'
 
 autodoc_mock_imports = ["torch", "apex", "mpi4py", "tensorboardX", "numpy", "cupy"]
+import mock
+for mod_name in autodoc_mock_imports:
+    sys.module[mod_name] = mock.Mock()
+
