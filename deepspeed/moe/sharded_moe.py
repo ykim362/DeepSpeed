b717f9aa1d1f28422de80d6073d80bf5629f2f75
Young Jin Kim <youki@microsoft.com>
2023-07-10 16:21:13 -0700

    Test expert choice routing

--- deepspeed/moe/sharded_moe.py
+++ deepspeed/moe/sharded_moe.py
@@ -185,94 +185,32 @@ def top1gating(logits: Tensor,
                use_rts: bool = True,
                use_tutel: bool = False) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
     """Implements Top1Gating on logits."""
-    if noisy_gate_policy == 'RSample':
-        logits_w_noise = logits + gumbel_rsample(logits.shape, device=logits.device)
     # everything is in fp32 in this function
-    gates = F.softmax(logits, dim=1)
+    # gates = F.softmax(logits, dim=1)
+    # TODO expert choice hard code
+    # sigmoid instead of softmax
+    gates = torch.sigmoid(logits)
 
     capacity = _capacity(gates, torch.tensor(capacity_factor), torch.tensor(min_capacity))
 
-    # Create a mask for 1st's expert per token
-    # noisy gating
-    indices1_s = torch.argmax(logits_w_noise if noisy_gate_policy == 'RSample' else gates, dim=1)
-    num_experts = int(gates.shape[1])
-    mask1 = F.one_hot(indices1_s, num_classes=num_experts)
-
+    # TODO expert choice hard code
     # mask only used tokens
     if used_token is not None:
-        mask1 = einsum("s,se->se", used_token, mask1)
+        gates = einsum("s,se->se", used_token, gates)
+
+    topk_score, topk_idx = torch.topk(
+        gates.transpose(0, 1), k=capacity, dim=-1, largest=True
+        # score, k=1, dim=-1, largest=True
+    )  # [.. x top_k]
 
     # gating decisions
-    exp_counts = torch.sum(mask1, dim=0).detach().to('cpu')
+    exp_counts = torch.sum(gates > 0, dim=0).detach().to('cpu')
 
-    # if we don't want to drop any tokens
-    if not drop_tokens:
-        new_capacity = torch.max(exp_counts).to(logits.device)
-        dist.all_reduce(new_capacity, op=dist.ReduceOp.MAX, group=dist.get_world_group())
-        capacity = new_capacity
+    # no aux loss for expert choice
+    l_aux = 0.0
 
-    # Compute l_aux
-    me = torch.mean(gates, dim=0)
-    ce = torch.mean(mask1.float(), dim=0)
-    l_aux = torch.sum(me * ce) * num_experts
-
-    # Random Token Selection
-    if use_rts:
-        uniform = exp_selection_uniform_map.get(logits.device)
-        if uniform is None:
-            uniform = torch.distributions.uniform.Uniform(low=torch.tensor(0.0, device=logits.device),
-                                                          high=torch.tensor(1.0, device=logits.device)).rsample
-            exp_selection_uniform_map[logits.device] = uniform
-
-        mask1_rand = mask1 * uniform(mask1.shape)
-    else:
-        mask1_rand = mask1
-
-    assert logits.shape[
-        0] >= min_capacity, "No. of tokens (batch-size) should be greater than min_capacity. Either set min_capacity to 0 or increase your batch size."
-
-    top_idx = _top_idx(mask1_rand, capacity)
-
-    new_mask1 = mask1 * torch.zeros_like(mask1).scatter_(0, top_idx, 1)
-    mask1 = new_mask1
-
-    if use_tutel:
-        # Tutel doesn't support index values masked with zero
-        # so we need to replace masked indices with -1
-        indices_mask = mask1.sum(dim=1) * num_experts - 1
-        indices1_s = torch.min(indices1_s, indices_mask)
-
-    # Compute locations in capacity buffer
-    if use_tutel:
-        locations1 = tutel_moe.fast_cumsum_sub_one(mask1)
-    else:
-        locations1 = torch.cumsum(mask1, dim=0) - 1
-
-    if use_tutel:
-        gates1_s = (gates * mask1).sum(dim=1)
-        locations1_s = torch.sum(locations1 * mask1, dim=1)
-        return l_aux, capacity, num_experts, [
-            indices1_s,
-        ], [
-            locations1_s,
-        ], [
-            gates1_s,
-        ], exp_counts
-
-    # Store the capacity location for each token
-    locations1_s = torch.sum(locations1 * mask1, dim=1)
-
-    # Normalize gate probabilities
-    mask1_float = mask1.float()
-    gates = gates * mask1_float
-
-    locations1_sc = _one_hot_to_float(locations1_s, capacity)
-    combine_weights = einsum("se,sc->sec", gates, locations1_sc)
-
-    dispatch_mask = combine_weights.bool()
-
-    return l_aux, combine_weights, dispatch_mask, exp_counts
-
+    # return l_aux, combine_weights, dispatch_mask, exp_counts
+    return l_aux, topk_score, topk_idx, exp_counts
 
 def top2gating(logits: Tensor, capacity_factor: float, min_capacity: int) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
     """Implements Top2Gating on logits."""
@@ -482,6 +420,10 @@ class MOELayer(Base):
         # group_size = kwargs['group_size'] if 'group_size' in kwargs.keys() else 1
         reshaped_input = input[0].reshape(-1, d_model)
 
+        num_tokens = reshaped_input.shape[0]
+
+        # TODO tutel doesn't work for expert choice
+        assert not self.use_tutel, "tutel doesn't work for expert choice"
         if self.use_tutel:
             self.l_aux, C, E, indices_, locations_, gates_, self.exp_counts = self.gate(reshaped_input, input[1], True)
             S, M = reshaped_input.size(0), reshaped_input.size(1)
@@ -492,7 +434,11 @@ class MOELayer(Base):
             dispatched_input = self._tutel_dispatcher.encode(reshaped_input)
         else:
             self.l_aux, combine_weights, dispatch_mask, self.exp_counts = self.gate(reshaped_input, input[1])
-            dispatched_input = einsum("sec,sm->ecm", dispatch_mask.type_as(input[0]), reshaped_input)
+            # TODO expert choice hard code
+            P = F.one_hot(dispatch_mask, num_classes=num_tokens).to(dtype=reshaped_input.dtype)
+            dispatched_input = torch.matmul(P, reshaped_input)
+            # TODO expert choice hard code
+            # dispatched_input = einsum("sec,sm->ecm", dispatch_mask.type_as(input[0]), reshaped_input)
 
         if self.wall_clock_breakdown:
             self.timers('falltoall').start()
@@ -538,7 +484,10 @@ class MOELayer(Base):
         if self.use_tutel:
             combined_output = self._tutel_dispatcher.decode(expert_output.view(E * C, M))
         else:
-            combined_output = einsum("sec,ecm->sm", combine_weights.type_as(input[0]), expert_output)
+            # TODO expert choice hard code
+            combined_output = torch.einsum("abl,ab,abd->ld", P, combine_weights, expert_output)
+            # TODO expert choice hard code
+            # combined_output = einsum("sec,ecm->sm", combine_weights.type_as(input[0]), expert_output)
 
         a = combined_output.reshape(input[0].shape)
 
